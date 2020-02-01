e554d76c6c7004164dc1fc1d4b618fc1b3d4f1f2
Ricardo Santos <ricardo.ams@hotmail.com>
2020-02-01 22:21:51 +0000

    deploy to heroku, connect mongodb database and starting html implemented

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
 import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"]= os.getenv("MONGO_DBNAME")
+app.config["MONGO_URI"]= os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "HI"
+@app.route('/testroute')
+def test():
+    return render_template("index.html", definicoes=mongo.db.Definitions.find())
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=os.environ.get('PORT'),
+    app.run(host=os.environ.get('IP',"0.0.0.0"),
+    port=os.environ.get('PORT',"5000"),
     debug=True)
 
