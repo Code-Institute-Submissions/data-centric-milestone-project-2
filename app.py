cc0f59557378d428ec4bb2870a1d574f3e7ffbf2
Ricardo Santos <ricardo.ams@hotmail.com>
2020-02-01 21:26:01 +0000

    app.py created with flask app instantiated

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "HI"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=os.environ.get('PORT'),
+    debug=True)
+
