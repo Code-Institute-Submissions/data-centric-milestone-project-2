276c86909164276a35c0e7e15863b41d5cd06f40
Ricardo Santos <ricardo.ams@hotmail.com>
2020-02-03 20:52:19 +0000

    GET and INSERT database function already tested and implemented

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"]= os.getenv("MONGO_DBNAME")
-app.config["MONGO_URI"]= os.getenv("MONGO_URI")
+app.config["MONGO_DBNAME"]= "DataCentricDB"
+app.config["MONGO_URI"]= "mongodb+srv://root:r00tUser@myfirstcluster-ur23i.mongodb.net/DataCentricDB?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
@@ -14,6 +14,16 @@ mongo = PyMongo(app)
 @app.route('/testroute')
 def test():
     return render_template("index.html", definicoes=mongo.db.Definitions.find())
+
+@app.route('/add')
+def add():
+    return render_template("add.html", domains=mongo.db.Domains.find())
+
+@app.route('/insert',methods=["POST"])
+def insert():
+    definitions= mongo.db.Definitions
+    definitions.insert_one(request.form.to_dict())
+    return redirect(url_for('test'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP',"0.0.0.0"),
