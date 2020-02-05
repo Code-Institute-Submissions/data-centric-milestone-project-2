2f24bddbe598578dda98ea535f01ce8345ac644a
Ricardo Santos <ricardo.ams@hotmail.com>
2020-02-05 01:18:06 +0000

    all CRUD operations implemented for all databases

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/testroute')
 def test():
-    return render_template("index.html", definicoes=mongo.db.Definitions.find())
+    return render_template("index.html", definitions=mongo.db.Definitions.find())
 
 @app.route('/add')
 def add():
@@ -24,6 +24,59 @@ def insert():
     definitions= mongo.db.Definitions
     definitions.insert_one(request.form.to_dict())
     return redirect(url_for('test'))
+
+@app.route('/update/<definitionid>', methods=['POST'])
+def update(definitionid):
+    definitions= mongo.db.Definitions
+    definitions.update({'_id':ObjectId(definitionid)},
+    {
+        'domain_name': request.form.get('domain_name'),
+        'term': request.form.get('term'),
+        'definition': request.form.get('definition')
+    })
+    return redirect(url_for('test'))
+
+@app.route('/edit/<definitionid>')
+def edit(definitionid):
+    definition=mongo.db.Definitions.find_one({'_id':ObjectId(definitionid)})
+    allcategories=mongo.db.Domains.find()
+    return render_template('edit.html',definition=definition,domains=allcategories)
+
+@app.route('/delete/<definitionid>')
+def delete(definitionid):
+    mongo.db.Definitions.remove({'_id':ObjectId(definitionid)})
+    return redirect(url_for('test'))
+
+@app.route('/domains')
+def domains():
+    return render_template('domains.html',domains=mongo.db.Domains.find())
+
+@app.route('/edit_domain/<domainid>')
+def edit_domain(domainid):
+    return render_template('editdomain.html', domain=mongo.db.Domains.find_one({'_id':ObjectId(domainid)}))
+
+@app.route('/update_domain/<domainid>',methods=['POST'])
+def update_domain(domainid):
+    mongo.db.Domains.update(
+        {'_id':ObjectId(domainid)},
+        {'domain_name':request.form.get('domain_name')})
+    return redirect(url_for('domains'))    
+
+@app.route('/delete_domain/<domainid>')
+def delete_domain(domainid):
+    mongo.db.Domains.remove({'_id':ObjectId(domainid)})
+    return redirect(url_for('domains'))
+
+@app.route('/insert_domain', methods=['POST'])
+def insert_domain():
+    domain=mongo.db.Domains
+    domain_doc={'domain_name':request.form.get('domain_name')}
+    domain.insert_one(domain_doc)
+    return redirect(url_for('domains'))
+
+@app.route('/add_domain')
+def add_domain():
+    return render_template('adddomain.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP',"0.0.0.0"),
